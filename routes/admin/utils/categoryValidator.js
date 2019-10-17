1a22d6fdfc2f8f9c0f56eaa9e03538d8c0de50a7
Christopher Brindle <chris.brindle@codeimmersives.com>
2019-10-17 13:13:44 -0400

    updated categoryValidator.js to check database to see if category being added already exists

--- routes/admin/utils/categoryValidator.js
+++ routes/admin/utils/categoryValidator.js
@@ -4,13 +4,25 @@ const categoryValidator = (req, res, next) => {
     if (req.body.name === '') {
         req.flash('errors', 'Field cannot be blank')
         res.redirect('/api/admin/add-category')
-    } else {
-        const newCat = new Category;
-        newCat.name = req.body.name;
-        newCat.save();
-        req.flash('success', 'Category added successfully');
-        res.redirect('/api/admin/add-category')
+        return
     }
+
+    Category.findOne({ name: req.body.name }, (err, result) => {
+        if (err) {
+            console.log(err);
+            return
+        } else if (!result) {
+            const newCat = new Category;
+            newCat.name = req.body.name;
+            newCat.save();
+            req.flash('success', 'Category added successfully');
+            res.redirect('/api/admin/add-category')
+            return
+        } else {
+            req.flash('errors', 'That category already exists')
+            res.redirect('/api/admin/add-category')
+        }
+    })
 }
 
 module.exports = categoryValidator
